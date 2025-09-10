b274faf0ead48854b99c5a8d646abb53afe5a229
Robert Starosta <robik2k4@gmail.com>
2025-09-10 17:24:04 +0200

    A bit of rollback

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,13 @@ mod modules;
 use modules::uart::uart;
 use modules::uart::uart_config;
 
-const PL011_BASE_ADDRESS: *mut uart_config::Registers = 0x3F000000 as _;
-
 #[no_mangle]
 pub extern "C" fn rust_main() {
 
-    let mut uart = unsafe { uart::Uart::new(PL011_BASE_ADDRESS) };
+    unsafe {
+        uart::init();
+        uart::puts("Hello, World!\n");
 
-    // unsafe {
-    //     uart::init();
-    //     uart::puts("rOS over UART...\n");
-    //
-    // }
-
-    loop {
-        if let Some(byte) = uart.read_byte() {
-            uart.write_byte(byte);
-            match byte {
-                b'\r' => uart.write_byte(b'\n'),
-                b'q' => break,
-                _ => continue,
-            }
-        }
     }
 }
 
