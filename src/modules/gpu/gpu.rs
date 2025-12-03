34f0860f83d86833fe29156eb96301b46c83b83b
Robert Starosta <robik2k4@gmail.com>
2025-12-03 01:03:52 +0100

    just pushing this to github. nothing much.

--- src/modules/gpu/gpu.rs
+++ src/modules/gpu/gpu.rs
@@ -1,1 +1,9 @@
 const MAILBOX_BASE_OFFSET: usize = 0xB880;
+
+const MAILBOX_READ: *mut u32 = (MAILBOX_BASE_OFFSET + 0x00) as *mut u32;
+const MAILBOX_STATUS : *mut u32 = (MAILBOX_BASE_OFFSET + 0x18) as *mut u32;
+const MAILBOX_WRITE:  *mut u32 = (MAILBOX_BASE_OFFSET + 0x20) as *mut u32;
+
+pub unsafe fn mailbox_read() {
+
+}
