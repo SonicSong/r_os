d76eba047f831dc3dab1c1c5dcbb870f9da422b7
Robert Starosta <robik2k4@gmail.com>
2025-12-09 03:01:40 +0100

    Trying to figure out gpu.rs

--- src/modules/gpu/gpu.rs
+++ src/modules/gpu/gpu.rs
@@ -1,8 +1,10 @@
-const MAILBOX_BASE_OFFSET: usize = 0xB880;
+const PERIPHERAL_BASE: usize = 0x3F000000;
+const MAILBOX_OFFSET: usize = 0x0000B880;
+const MAILBOX_BASE: usize = PERIPHERAL_BASE + MAILBOX_OFFSET;
 
-const MAILBOX_READ: *mut u32 = (MAILBOX_BASE_OFFSET + 0x00) as *mut u32;
-const MAILBOX_STATUS : *mut u32 = (MAILBOX_BASE_OFFSET + 0x18) as *mut u32;
-const MAILBOX_WRITE:  *mut u32 = (MAILBOX_BASE_OFFSET + 0x20) as *mut u32;
+const MAILBOX_READ: *mut u32 = (MAILBOX_BASE + 0x00) as *mut u32;
+const MAILBOX_STATUS : *mut u32 = (MAILBOX_BASE + 0x18) as *mut u32;
+const MAILBOX_WRITE:  *mut u32 = (MAILBOX_BASE + 0x20) as *mut u32;
 
 pub unsafe fn mailbox_read() {
 
--- src/modules/uart/uart_pl011.rs
+++ src/modules/uart/uart_pl011.rs
@@ -34,32 +34,6 @@ const UART0_IMSC : *mut u32 = (UART0_BASE + 0x38) as *mut _;
 /// Interupt Clear Register
 const UART0_ICR : *mut u32 = (UART0_BASE + 0x44) as *mut _;
 
-bitflags! {
-    #[repr(transparent)]
-    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-
-    pub struct Flags: u32 {
-        /// Clear to send.
-        const CTS = 1 << 0;
-        /// Data set ready.
-        const DSR = 1 << 1;
-        /// Data carrier detect.
-        const DCD = 1 << 2;
-        /// UART busy transmitting data.
-        const BUSY = 1 << 3;
-        /// Receive FIFO is empty.
-        const RXFE = 1 << 4;
-        /// Transmit FIFO is full.
-        const TXFF = 1 << 5;
-        /// Receive FIFO is full.
-        const RXFF = 1 << 6;
-        /// Transmit FIFO is empty.
-        const TXFE = 1 << 7;
-        /// Ring indicator.
-        const RI = 1 << 8;
-    }
-}
-
 pub unsafe fn init() {
     // Disable UART
     write_volatile(UART0_CR, 0);
